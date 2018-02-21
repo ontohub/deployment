99d7ab471993a76a2494ed84b10f5d69c240f737
Tom Gehrke <phyrog@cognitive-coding.com>
2018-02-21 13:08:25 +0100

    Deploy git-shell

---
+++ config/mixins/applications/git-shell.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+Rake::Task['load:defaults'].invoke
+Rake::Task['load:defaults'].clear
+mixin('ruby_project_requirements')
+Rake::Task['load:defaults'].reenable
+Rake::Task['load:defaults'].invoke
+
+set :application, 'git-shell'
+set :repo_url, 'https://github.com/ontohub/git-shell.git'
+
+mixin('ruby_project_config')
+
+# Default value for :linked_files is []
+# append :linked_files, 'config/settings.yml'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log'
